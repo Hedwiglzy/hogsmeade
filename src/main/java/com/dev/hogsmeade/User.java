d581af3dd786936d35ccc6957e9b355c2cbd819c
Hedwig <Hedwiglzy@163.com>
2023-10-19 21:36:06 +0800

    commit

--- src/main/java/com/dev/hogsmeade/User.java
+++ src/main/java/com/dev/hogsmeade/User.java
@@ -7,8 +7,8 @@ import jakarta.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
-  // @Id
-  // @GeneratedValue(strategy=GenerationType.AUTO)
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
   private Integer id;
 
   private String name;
