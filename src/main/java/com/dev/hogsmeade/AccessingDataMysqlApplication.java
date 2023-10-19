1dec2a67a64d8fd489dac3ee878e5a92fd2cc30f
hedwig <hedwiglzy@163.com>
2023-10-19 17:09:29 +0800

    初始化

---
+++ src/main/java/com/dev/hogsmeade/AccessingDataMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.dev.hogsmeade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMysqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AccessingDataMysqlApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dev/hogsmeade/HogsmeadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.hogsmeade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogsmeadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
